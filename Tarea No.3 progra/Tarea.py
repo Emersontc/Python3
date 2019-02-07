7e010368037edb034674f01038caed2e9981e9af
cesartista12 <cesarleotc29@gmail.com>
2019-02-06 18:00:43 -0600

    Actividad 3

---
+++ Tarea No.3 progra/Tarea.py
@@ -0,0 +1,119 @@
+#ejecicio 1
+#Solicitar al usuario y selecione una opcion.
+#1. solicitar 2 numeros y elevar el primer numero elevado al 2do numero
+#2. solicitar 3 numero y elevar el 1ro al 2do y el resultado elevarlo al 3ro
+#print ("Bienvenido al programa de elevación".center(50,"-"))
+
+#opcion=input("""1-Ingrese 2 numero para elevarlo al primero por el segundo.
+#2-Ingrese 3 numero y se elevara el 1ro al 2do y el resuldo se elevara al 3ro.""")
+
+#if opcion == "1":
+#    print ("Ingrese 2 numero para elevarlo el primero por el segundo")
+#    valor1 = input ("Ingrese el primer numero:")
+#    valor2 = input ("Ingrese el segundo numero:")
+#    elevar = int (valor1) ** int (valor2)
+#    print ("La elevacion es {}".format(elevar))
+#elif opcion == "2":
+#    print ("Ingrese 3 numero y elevar el 1ro al 2do y el resultado se elevara al 3ro")
+#    valor1 = input ("Ingrese el primer numero:")
+#    valor2 = input ("Ingrese el segundo numero:")
+#    valor3 = input ("Ingrese el tercer numero:")
+#    elevar = int (valor1) ** int (valor2)
+#    elevacion = int (valor3) ** int (elevar)
+#    print ("La elevacion es {}".format(elevacion))
+
+
+
+
+
+
+
+
+
+
+
+#ejercicio 2
+#Ingresar un valor y mostrar los numeros desde el -10 al numero ingresado
+#print ("Bienvenido al programa".center(50,"-"))
+#numero = int(input("Ingrese un numero:"))
+#valor = -10
+#while valor <= numero:
+#   print(valor)
+#   valor+= 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ejercicio 3
+#realizar el promedio de 5 notas utilizando el ciclo for
+#suma = 0
+#print ("Bienvenido al programa para sacar el promedio".center(50,"-"))
+#print ("Ingrese 5 notas para sacar el promedio")
+#for i in range (5):
+#    nota = int(input("Ingrese una nota:."))
+#    suma = suma + nota
+#promedio = suma / 5
+#print ("El promedio es: {}".format(promedio))
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ejercico 4
+#realizar el promedio de n notas utilizando el for
+nota = 0
+suma = 0
+print ("Bienvenido al programa para sacar el promedio".center(50,"-"))
+
+print ("Ingrese varias notas para sacar el promedio")
+
+cantida = int(input("Ingrese las notas que quieras sacar el promedio:"))
+for i in range (cantida):
+    nota = int(input("Ingrese una nota:."))
+    suma = suma + nota
+promedio = suma / cantida
+print ("El promedio es: {}".format(promedio))
+
+
+
+
+
+
+
+
+#ejercico 5
+#realizar la multiplicacion de 2 numero sin utilizar la multiplicacion
+#print ("Bienvenido al programa para multiplicar".center(50,"-"))
+#print ("Ingrese 2 notas para multiplicarlas")
+
+#nota1 = int(input("Ingrese una nota:"))
+#nota2 = int(input("Ingrese la siguiente nota:"))
+#multiplicar = nota1 + nota2
+##print ("La multiplicacion es: {}".format(multiplicar))
+
+
+
+
+
+
